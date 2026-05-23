8c10ec77fdec6aad152a04fadcc0d95d7706bd72
Pedro Jesús Soler <pedro.solermor@gmail.com>
2026-05-24 01:53:58 +0200

    Modify benchmark to have all the parameters in one notebook

--- rftrack/structure_volume.py
+++ rftrack/structure_volume.py
@@ -123,40 +123,40 @@ def create_lebt(B0):
     lebt.add_ref(drift5, 0, 0, z)
     z += 0.722
 
-    lebt.add_ref(collx, 0, 0, z)
-    z += 0.002
-
-    lebt.add_ref(drift_col, 0, 0, z)
-    z += 0.002
-
-    lebt.add_ref(colly, 0, 0, z)
-    z += 0.002
-
-    lebt.add_ref(drift6, 0, 0, z)
-    z += 0.194
-
-    lebt.add_ref(quad1, 0, 0, z)
-    z += 0.14
-
-    lebt.add_ref(drift7, 0, 0, z)
-    z += 0.092
-
-    lebt.add_ref(quad2, 0, 0, z)
-    z += 0.14
-
-    lebt.add_ref(drift8, 0, 0, z)
-    z += 0.092
-
-    lebt.add_ref(quad3, 0, 0, z)
-    z += 0.14
-
-    lebt.add_ref(drift9, 0, 0, z)
-    z += 0.9101
-
-    lebt.add_ref(sol, 0, 0, z)
-    z += 0.2574
-
-    lebt.add_ref(drift10, 0, 0, z)
-    z += 0.139
+    #lebt.add_ref(collx, 0, 0, z)
+    #z += 0.002
+#
+    #lebt.add_ref(drift_col, 0, 0, z)
+    #z += 0.002
+#
+    #lebt.add_ref(colly, 0, 0, z)
+    #z += 0.002
+#
+    #lebt.add_ref(drift6, 0, 0, z)
+    #z += 0.194
+#
+    #lebt.add_ref(quad1, 0, 0, z)
+    #z += 0.14
+#
+    #lebt.add_ref(drift7, 0, 0, z)
+    #z += 0.092
+#
+    #lebt.add_ref(quad2, 0, 0, z)
+    #z += 0.14
+#
+    #lebt.add_ref(drift8, 0, 0, z)
+    #z += 0.092
+#
+    #lebt.add_ref(quad3, 0, 0, z)
+    #z += 0.14
+#
+    #lebt.add_ref(drift9, 0, 0, z)
+    #z += 0.9101
+#
+    #lebt.add_ref(sol, 0, 0, z)
+    #z += 0.2574
+#
+    #lebt.add_ref(drift10, 0, 0, z)
+    #z += 0.139
 
     return lebt
